1e550c698fe473f8cc67debfc0470fa361c08ff4
Shani Halali <shanihlli1234@gmail.com>
2025-12-02 16:38:16 +0200

    Fix: extended User model with score and answer tracking fields

--- controllers/userController.js
+++ controllers/userController.js
@@ -52,3 +52,4 @@ exports.getUserById = async(req, res) => {
         });
     }
 }
+
--- models/User.js
+++ models/User.js
@@ -5,7 +5,10 @@ const userSchema = new mongoose.Schema(
     userId: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     learningLanguage: { type: String, required: true },
-    difficulty: { type: String, required: true }
+    difficulty: { type: String, required: true },
+    totalScore: {type: Number, default: 0},
+    numOfWrongAnswers: {type: Number, default: 0},
+    NumOfCorrectAnswers: {type: Number, default: 0}
   },
   { timestamps: true }
 );
